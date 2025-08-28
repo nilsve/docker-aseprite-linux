9cc2a4049230977c36cd10207a0b1f57e6a6a1dd
nilsve <nils.vaneijk@outlook.com>
2025-08-28 15:53:36 +0200

    Update Aseprite to 1.3.15.2

--- compile.sh
+++ compile.sh
@@ -3,12 +3,23 @@
 # Fail on errors
 set -e
 
+ASEPRITE_VERSION=v1.3.15.2
+SKIA_VERSION=aseprite-m124
+
 echo "Download and compile Skia & other dependencies"
 cd /dependencies
 
 if [ ! -d "/dependencies/depot_tools" ]
 then
+  echo "Cloning depot_tools"
   git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+  git checkout main
+else
+  cd depot_tools
+  echo "Checking out latest depot_tools"
+  git checkout main
+  git pull
+  cd ..
 fi
 
 export PATH="${PWD}/depot_tools:${PATH}"
@@ -16,7 +27,14 @@ gclient
 
 if [ ! -d "/dependencies/skia" ]
 then
-  git clone -b aseprite-m102 https://github.com/aseprite/skia.git
+  echo "Cloning skia"
+  git clone -b "${SKIA_VERSION}" https://github.com/aseprite/skia.git
+else
+  echo "Updating to latest Skia version"
+  cd skia
+  git fetch
+  git checkout "${SKIA_VERSION}"
+  cd ..
 fi
 
 cd skia
@@ -28,12 +46,18 @@ echo "Compiling skia"
 gn gen out/Release-x64 --args="is_debug=false is_official_build=true skia_use_system_expat=false skia_use_system_icu=false skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false skia_use_sfntly=false skia_use_freetype=true skia_use_harfbuzz=true skia_pdf_subset_harfbuzz=true skia_use_system_freetype2=false skia_use_system_harfbuzz=false"
 ninja -C out/Release-x64 skia modules
 
-echo "Download Aseprite and compile"
 cd /output
 
 if [ ! -d "/output/aseprite" ]
 then
-  git clone -b v1.2.40 --recursive https://github.com/aseprite/aseprite.git
+  echo "Cloning Aseprite"
+  git clone -b "${ASEPRITE_VERSION}" --recursive https://github.com/aseprite/aseprite.git
+else
+  echo "Updating to latest Aseprite version"
+  cd aseprite
+  git fetch
+  git switch "${ASEPRITE_VERSION}" --detach
+  cd ..
 fi
 
 cd aseprite
